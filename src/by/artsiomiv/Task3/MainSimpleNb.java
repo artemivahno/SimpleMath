9f80246956b2b27de672023c14c0e18b651c351b
artemwork <said.kirill@gmail.com>
2018-12-05 15:45:56 +0300

    Task 3

---
+++ src/by/artsiomiv/Task3/MainSimpleNb.java
@@ -0,0 +1,9 @@
+package by.artsiomiv.Task3;
+
+//Каков самый большой делитель числа 600851475143, являющийся простым числом?
+//http://euler.jakumo.org/problems/view/3.html
+
+public class MainSimpleNb {
+
+
+}
